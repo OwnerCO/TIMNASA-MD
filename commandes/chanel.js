010cb44b2f76ff073dcb1a4dcb238cd9a93054b5
Timnasa <Halisimd212@gmail.com>
2024-11-27 07:04:50 +0300

    Update chanel.js

--- commandes/chanel.js
+++ commandes/chanel.js
@@ -9,7 +9,7 @@ zokou({ nomCom: "channel", reaction: "😌", nomFichier: __filename }, async (de
     let varmess = z + d;
     var lien = 'https://whatsapp.com/channel/0029VarwsIB8F2pG9QWm3n1ZjweHxKQuJP6qnjLM31';  // Remplacez cet URL par le lien que vous souhaitez envoyer
     await zk.sendMessage(dest, { text: varmess + "\n" + lien });
-});
+});https://whatsapp.com/channel/0029VajweHxKQuJP6qnjLM31
 
 console.log("mon test");
 
